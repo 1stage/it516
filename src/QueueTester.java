d65d1fa9e530c98f0dae6fcf9072731efd60b077
Sean P. Harrington <spharrin@callutheran.edu>
2023-06-03 10:15:29 -0700

    Update QueueTester.java

--- src/QueueTester.java
+++ src/QueueTester.java
@@ -25,25 +25,27 @@ public class QueueTester {
 
         StdOut.println("Enqueue people...");
         people.enqueue("Dani Chong");                                      // Use the enqueue() function
-        StdOut.println(people.peek() + " ");                               // Print the last person we added
+        StdOut.println("  Dani Chong");                                    // Print the last person we added
         people.enqueue("Barbara Perez");                                   // Use the enqueue() function
-        StdOut.println(people.peek() + " ");                               // Print the last person we added
+        StdOut.println("  Barbara Perez");                                 // Print the last person we added
         people.enqueue("Ritesh Solanki");                                  // Use the enqueue() function
-        StdOut.println(people.peek() + " ");                               // Print the last person we added
+        StdOut.println("  Ritesh Solanki");                                // Print the last person we added
         people.enqueue("Matthew Gold");                                    // Use the enqueue() function
-        StdOut.println(people.peek() + " ");                               // Print the last person we added
+        StdOut.println("  Matthew Gold");                                  // Print the last person we added
         people.enqueue("Alam Simari");                                     // Use the enqueue() function
-        StdOut.println(people.peek() + " ");                               // Print the last person we added
+        StdOut.println("  Alam Simari");                                   // Print the last person we added
         StdOut.println("Queue now has " + people.size() + " people in it."); // Use the size() function
+        StdOut.println();
 
         StdOut.println("Iterating through Queue people...");
         for (String thisPerson : people)                                    // Use the Iterator parent class to print each person
-            StdOut.println(thisPerson);
+            StdOut.println("  " + thisPerson);
         StdOut.println("Queue still has " + people.size() + " people in it."); // We iterated, not dequeued!
+        StdOut.println();
 
         StdOut.println("Dequeue people...");
         while (!people.isEmpty()) {                                         // Use while loop until Queue isEmpty() is true
-            StdOut.println(people.dequeue() + " ");                         // Dequeue each person to the output
+            StdOut.println("  " + people.dequeue());                        // Dequeue each person to the output
         }
         StdOut.println();                                                   // Blank line
         StdOut.println("Queue now has " + people.size() + " people in it."); // Show that Queue has zero people in it now
