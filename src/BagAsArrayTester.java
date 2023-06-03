78ccca95bb421bc5da0043bb3fcefee73c250331
Sean P. Harrington <spharrin@callutheran.edu>
2023-06-03 09:35:37 -0700

    Update BagAsArrayTester.java

--- src/BagAsArrayTester.java
+++ src/BagAsArrayTester.java
@@ -1,20 +1,26 @@
+import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.StdOut;
 
 public class BagAsArrayTester {
     public static void main(String[] args) {
-        BagAsArray<String> things = new BagAsArray<>(4);                    // Make a new empty BagAsArray called "things"
+        Bag<String> things = new Bag<String>();                             // Make a new empty Bag called "things"
         StdOut.println("Is this new Bag empty? " + things.isEmpty());       // Use the isEmpty() function
         things.add("rock");                                                 // Use the add() function
+        StdOut.println("rock");                                             // Print the last thing added
         things.add("paper");                                                // Use the add() function
+        StdOut.println("paper");                                            // Print the last thing added
         things.add("scissors");                                             // Use the add() function
+        StdOut.println("scissors");                                         // Print the last thing added
         things.add("lizard");                                               // Use the add() function
+        StdOut.println("lizard");                                           // Print the last thing added
         things.add("Spock");                                                // Use the add() function
+        StdOut.println("Spock");                                            // Print the last thing added
         StdOut.println("Bag now has " + things.size() + " items in it.");   // Use the size() function
 
         StdOut.println("Iterating through Bag items...");
         String allItems = "";                                               // Create a new String to hold the output
-        for (String thisItem : things)                                      // Use the Iterator parent class to go through each item
-            allItems += (thisItem + " ");                                   // Concatenate each item to the allItems String
+        for (String thisItem : things)                                      // Use the Iterator parent class to print each item
+            StdOut.println(thisItem);
         StdOut.println(allItems);                                           // Print out the allItems String
         StdOut.println();                                                   // Blank line
 
